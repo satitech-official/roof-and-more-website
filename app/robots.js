bb60b8427ed5e9ca30afe1efe23ef5a5ad5eb4b6
SatiTech <www.satitechinfo@gmail.com>
2026-07-24 18:51:39 +0530

    Make robots metadata static for GitHub Pages

--- app/robots.js
+++ app/robots.js
@@ -1,5 +1,9 @@
+export const dynamic = "force-static";
+
 export default function robots() {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://roof-and-more.example";
+  const baseUrl =
+    process.env.NEXT_PUBLIC_SITE_URL ||
+    "https://satitech-official.github.io/roof-and-more-website";
 
   return {
     rules: {
